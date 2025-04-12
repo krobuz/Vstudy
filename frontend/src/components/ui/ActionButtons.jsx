8b55caa582fa9c09a4bcbf661316502a6c5a6afc
krobuz <minhbui12.2003@gmail.com>
2025-04-13 01:14:03 +0700

    fix conflict branch

--- frontend/src/components/ui/ActionButtons.jsx
+++ frontend/src/components/ui/ActionButtons.jsx
@@ -1,10 +1,6 @@
 const ActionButtons = ({ onAdd }) => {
     return (
-<<<<<<< HEAD
       <button onClick={onAdd} className="text-white px-4 py-2 m3">Thêm</button>
-=======
-      <button onClick={onAdd} className="bg-green-500 text-white px-4 py-2 m3">Thêm</button>
->>>>>>> develop
     );
   };
 
